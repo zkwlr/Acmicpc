37a2bc3d3adbec772ebfc658818e2a9d62731498
zkwlr <bluefrog2k@yonsei.ac.kr>
2024-05-20 14:00:19 +0900

    a

--- 10814.py
+++ 10814.py
@@ -7,7 +7,11 @@ n = int(input().strip())
 
 ls = [[x for x in input().split()] for _ in range(n)]
 
-ls.sort(key=lambda x: x[0])
+for i in range(n):
+    ls[i][0] = int(ls[i][0])
+    ls[i].append(i)
+
+ls.sort(key=lambda x: (x[0], x[2]))
 
 for i in ls:
     print(i[0], i[1])
