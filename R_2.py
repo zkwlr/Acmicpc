e217c361c874d0eb71ecc60130f4e99057ba1fc5
zkwlr <bluefrog2k@yonsei.ac.kr>
2024-05-27 03:17:14 +0900

    5/26

---
+++ R_2.py
@@ -0,0 +1,4 @@
+# B4
+
+r1, s = map(int, input().split())
+print(2 * s - r1)
---
+++ 부녀회장이_될테야.py
@@ -0,0 +1,8 @@
+import sys 
+input = sys.stdin.readline
+
+t = int(input())
+
+for i in range(t):
+    k,n = int(input()), int(input())
+    ls = 
