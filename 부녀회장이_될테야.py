9b78274e7f80b6ac21ac451f32c1fe64e0f288dd
zkwlr <bluefrog2k@yonsei.ac.kr>
2024-05-28 17:52:27 +0900

    5/28

--- 부녀회장이_될테야.py
+++ 부녀회장이_될테야.py
@@ -1,8 +1,17 @@
-import sys 
+import sys
+
 input = sys.stdin.readline
 
 t = int(input())
 
+ls = [[0] * 15 for _ in range(15)]
+
+for i in range(1, 15):
+    ls[0][i] = i
+
 for i in range(t):
-    k,n = int(input()), int(input())
-    ls = 
+    k, n = int(input()), int(input())
+    for i in range(1, k + 1):
+        for j in range(1, n + 1):
+            ls[i][j] = ls[i - 1][j] + ls[i][j - 1]
+    print(ls[k][n])
