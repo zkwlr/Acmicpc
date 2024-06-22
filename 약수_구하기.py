7b6165b223ae89fc425915d31a092bba980d2052
zkwlr <bluefrog2k@yonsei.ac.kr>
2024-06-22 09:40:59 +0900

    6/20

---
+++ 약수_구하기.py
@@ -0,0 +1,17 @@
+# B3
+
+import sys
+
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+cnt = 0
+
+for i in range(1, n + 1):
+    if n % i == 0:
+        cnt += 1
+    if cnt == k:
+        print(i)
+        break
+if cnt < k:
+    print(0)
